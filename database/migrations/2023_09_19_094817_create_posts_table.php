ef73cdca8b2a6371e9b622220a94df812cce1f71
nikawa2161 <nikawa.2161.t@gmail.com>
2023-10-17 18:16:58 +0900

    初めてのコミット

---
+++ database/migrations/2023_09_19_094817_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            //カラムの定義  形式は　　$table->型(‘カラム名’);
+            $table->bigIncrements('id'); //AI(auto increment) 自動的に連番で入る
+            $table->string('title'); //varcharはlaravelでは"string"で定義
+            $table->text('body');
+            $table->integer('category_id');
+            $table->timestamps(); //created_at と updated_at カラムが自動生成される
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
